7ac47b71a9918125c156f6f5b5096a90bd9b5b4a
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2017-12-01 20:18:40 -0500

    View Controllers and Files Created. Next Step is connecting all outlets and searchbar

---
+++ AC-iOS-EpisodesFromOnline-HW/SeriesAPIManager.swift
@@ -0,0 +1,9 @@
+//
+//  SeriesAPIManager.swift
+//  AC-iOS-EpisodesFromOnline-HW
+//
+//  Created by Richard Crichlow on 12/1/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import Foundation
