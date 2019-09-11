c976e0bbd51d76256dccf7b409261ce6b262cf09
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro.local>
2019-09-10 21:05:00 -0400

    Created link structs and updated plist http settings

--- Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ImageHelper.swift
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ImageHelper.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import UIKit
+
+class ImageHelper {
+    
+    private init() {}
+    
+    static let shared = ImageHelper()
+    
+    
+    func getImage(urlStr: String, completionHandler: @escaping (Result<UIImage, Error>) -> Void) {
+        
+        guard let url = URL(string: urlStr) else {
+            completionHandler(.failure(ErrorHandling.badURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            guard error == nil else {
+                completionHandler(.failure(ErrorHandling.noData))
+                return
+            }
+            guard let data = data else {
+                completionHandler(.failure(ErrorHandling.noData))
+                return
+            }
+            
+            guard let image = UIImage(data: data) else {
+                completionHandler(.failure(ErrorHandling.noData))
+                return
+            }
+            
+            completionHandler(.success(image))
+            
+            
+            }.resume()
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
