ac12ba0c43c003bdb830ab2bfef027bf13adabfd
Sam <samroman3@gmail.com>
2019-09-11 10:14:25 -0400

    Initial commit

--- Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/EpisodesDetailViewController.swift
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/EpisodesDetailViewController.swift
@@ -71,19 +71,99 @@ class EpisodesDetailViewController: UIViewController, UITableViewDelegate, UITab
         episodeTableView.dataSource = self
         loadEpisode(id: endPoint?.id)
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+
+/*
+ import UIKit
+ 
+ class EpisodeViewController: UIViewController {
+ var show: ShowsWrapper?
+ @IBOutlet weak var showImage: UIImageView!
+ @IBOutlet weak var showName: UILabel!
+ @IBOutlet weak var showDescrip: UITextView!
+ 
+ var episodes = [Episodes](){
+ didSet {
+ episodeTable.reloadData()
+ setUpLabelsShow()
+ }
+ }
+ @IBOutlet weak var episodeTable: UITableView!
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ episodeTable.delegate = self
+ episodeTable.dataSource = self
+ getData()
+ }
+ func getData(){
+ Episodes.getEpisode(id: (show?.show.id)!){ (result) in
+ switch result {
+ case .failure(let error):
+ print(error)
+ case .success(let episode):
+ DispatchQueue.main.async{
+ return self.episodes = episode
+ }
+ }
+ }
+ }
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ guard let epVc = segue.destination as? DetailViewController else {
+ fatalError("Unexpected segue")
+ }
+ guard let selectedIndexPath = episodeTable.indexPathForSelectedRow
+ else { fatalError("No row selected") }
+ epVc.episode = episodes[selectedIndexPath.row]
+ }
+ func setUpLabelsShow(){
+ showName.text = show?.show.name
+ showDescrip.text = show?.show.fixedSummary
+ if let image = show?.show.image?.original{
+ ImageHelper.shared.fetchImage(urlString: image) { (result) in
+ DispatchQueue.main.async {
+ switch result {
+ case .failure(let error):
+ print(error)
+ case .success(let image):
+ self.showImage.image = image
+ }
+ }
+ }
+ }}
+ }
+ 
+ extension EpisodeViewController: UITableViewDelegate, UITableViewDataSource{
+ func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+ return episodes.count
+ }
+ 
+ func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+ let cell = episodeTable.dequeueReusableCell(withIdentifier: "episodeCell", for: indexPath) as? episodeTableViewCell
+ let epi = episodes[indexPath.row]
+ if let image = epi.image?.original{
+ ImageHelper.shared.fetchImage(urlString: image) { (result) in
+ DispatchQueue.main.async {
+ switch result {
+ case .failure(let error):
+ print(error)
+ case .success(let image):
+ cell?.episodeImage.image = image
+ }
+ }
+ }
+ }
+ 
+ cell?.episodeName.text = epi.name
+ cell?.episodeNum.text = epi.episodeFormat
+ cell?.episodeDescrip.text = epi.summary
+ return cell!
+ }
+ func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+ return 140
+ }
+ }*/
