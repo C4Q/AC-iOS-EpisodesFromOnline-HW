9c899e76ff498929f30c38d8478516945adb8ef4
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-09-12 16:39:32 -0400

    Creates second view controller file

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/Controllers/SingleShowViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SingleShowViewController.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Mariel Hoepelman on 9/12/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class SingleShowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
