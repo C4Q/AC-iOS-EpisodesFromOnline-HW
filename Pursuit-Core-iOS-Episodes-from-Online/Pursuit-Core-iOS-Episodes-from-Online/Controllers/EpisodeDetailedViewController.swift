f335a8b98091b73c14a3b4f7648111683af92935
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-09-12 16:41:10 -0400

    Creates third view controller file

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/Controllers/EpisodeDetailedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EpisodeDetailedViewController.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Mariel Hoepelman on 9/12/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeDetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
