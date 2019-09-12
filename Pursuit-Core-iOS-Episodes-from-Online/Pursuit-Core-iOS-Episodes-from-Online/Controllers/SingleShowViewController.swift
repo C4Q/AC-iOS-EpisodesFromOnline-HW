66378954d5b289d3ce62b1ef8c126f4a0540a977
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-09-12 16:51:33 -0400

    Conforms SingleShowViewController to data source and delegate protocols

--- Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/Controllers/SingleShowViewController.swift
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/Controllers/SingleShowViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class SingleShowViewController: UIViewController {
 
+    @IBOutlet weak var SingleShowTableView: UITableView!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
     }
 
+}
+
+extension SingleShowViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //cell id = ShowListTVC
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension SingleShowViewController: UITableViewDelegate {
+    
 }
