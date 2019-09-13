d6c02ac6d2ca70c0983bf2ccbe584c259e4e5bd9
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-09-12 22:19:21 -0400

    Binary files changes.

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/Models/ImageModel.swift
@@ -0,0 +1,14 @@
+//
+//  ImageModel.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Mariel Hoepelman on 9/12/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import Foundation
+
+struct Image: Codable {
+    let medium: String
+    let original: String
+}
