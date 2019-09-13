c233c106334f6406773a647adec40ad303b3dc28
hildyabreu114 <hildyabreu@pursuit.org>
2019-09-13 12:33:29 -0400

    updated storyboard, ApiClientFile, show and episode models

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/EpisodeDetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  EpisodeDetailViewController.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by hildy abreu on 9/11/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeDetailViewController: UIViewController {
+
+   
+    
+    
+    @IBOutlet weak var detailEpisodeNumberLabel: UILabel!
+    
+    
+    @IBOutlet weak var detailSeasonlabel: UILabel!
+    
+    
+    @IBOutlet weak var detailEpisodeNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var detailImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
