a5b8e21a77d14a3a7e8c080e043ebc46eb1b8d7b
CameronRivera <cameronrivera@pursuit.org>
2019-12-07 22:04:07 -0500

    Created NetworkHelper Class and Set up UI

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/DetailedEpisodeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailedEpisodeViewController.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Cameron Rivera on 12/7/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class DetailedEpisodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
--- Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ViewController.swift
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var data = Data(){
+        didSet{
+            print(data)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUp()
+    }
+    
+    private func setUp(){
+        NetworkHelper.shared.getData(using: "http://api.tvmaze.com/search/shows?q=girls") { result in
+            switch result{
+            case .failure(let error):
+                print("Encountered Error: \(error)")
+            case .success(let validData):
+                self.data = validData
+            }
+        }
     }
 
 
