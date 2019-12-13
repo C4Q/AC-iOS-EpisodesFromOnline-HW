4db38b8ced8d98d50350ed43eaa198859c18da9f
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2019-12-12 19:39:52 -0500

    Setting up first view controller

--- Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ShowsViewController.swift
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ShowsViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ShowsViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
